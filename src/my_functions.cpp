89a63589b5817daa53470751ea72aa4dbf7041a1
Your Name <you@example.com>
2023-08-07 10:02:39 +0200

    asdasd

--- src/my_functions.cpp
+++ src/my_functions.cpp
@@ -1,20 +1,24 @@
 #include "my_functions.h"
-#include <iostream>     // std::cout
-#include <algorithm>    // std::sort
-#include <vector>       // std::vector
-bool myFunction(std::vector<int>& nums) {
-  std::sort (nums.begin(), nums.end());     
+#include <iostream>  // std::cout
+#include <algorithm> // std::sort
+#include <vector>    // std::vector
+#include <unordered_map>
+bool myFunction2(std::vector<int> &nums)
+{
+  std::sort(nums.begin(), nums.end());
   // print out content:
   bool first = true;
   bool retrnvalue = false;
-  for (std::vector<int>::iterator it=nums.begin(); it!=nums.end(); ++it)
+  for (std::vector<int>::iterator it = nums.begin(); it != nums.end(); ++it)
   {
-    if(first==false){
+    if (first == false)
+    {
       int x = *it;
       --it;
       int y = *it;
       ++it;
-      if(x==y) retrnvalue=true;
+      if (x == y)
+        retrnvalue = true;
     }
     else
     {
@@ -24,4 +28,14 @@ bool myFunction(std::vector<int>& nums) {
   return retrnvalue;
 }
 
-
+bool myFunction(std::vector<int> &nums, int target)
+{
+  std::unordered_map<int, int> hashmap;
+  std::vector<int> result;
+  for (int i = 0; i < nums.size(); i++)
+  {
+    int complement = target - nums[i];
+        
+  }
+  return true;
+}
